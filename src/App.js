fd441df052139404f37adb2767708dc418251d8b
gilsgstav <gilstav@hotmail.com>
2019-07-02 21:17:37 -0700

    recommit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React by day!! gxs1
+          Learn React by day
         </a>
       </header>
     </div>
